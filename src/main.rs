4284dc628ba6c33491d7cb3c2f981bc220a23a73
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2024-02-24 13:52:26 -0600

    feat: Window dimension constants

--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,58 @@ use sfml::system::{
 };
 use sfml::window::{
     ContextSettings,
-    VideoMode,
     Event,
-    Style
+    Style,
+    VideoMode
 };
 use sfml::graphics::{
-    RenderWindow,
-    RenderTarget,
-    CircleShape,
     Color,
+    Font,
+    RenderTarget,
+    RenderWindow,
+    Text,
     Transformable,
-    Shape
 };
 
 const FPS: i64 = 30;
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 400;
 
 fn main() {
     let frame_time = Time::seconds(1.) / FPS;
     let mut clock = Clock::start();
+
+    let font = match Font::from_file("resources/fonts/arial.ttf") {
+        Some(font) => font,
+        None => {
+            panic!("Failed to read font file!");
+        }
+    };
     
+    let mut text = Text::new("Snow goes here", &font, 24);
+
+    text.set_position(Vector2f::new((WIDTH as f32) / 2., (HEIGHT as f32) / 2.));
+    text.set_fill_color(Color::GREEN);
+
     // Create the window of the application
-    let mut window : RenderWindow = RenderWindow::new(VideoMode::new(800, 600, 32),
-                                                      "SFML Example",
+    let mut window : RenderWindow = RenderWindow::new(VideoMode::new(WIDTH, HEIGHT, 32),
+                                                      "Snow Blitz",
                                                       Style::CLOSE,
                                                       &ContextSettings::default());
 
-    // Create a CircleShape
-    let mut circle = CircleShape::new(1.0, 12);
-    circle.set_radius(30.);
-    circle.set_fill_color(Color::BLUE);
-    circle.set_position(Vector2f::new(100., 100.));
-
     while window.is_open() {
         if clock.elapsed_time() >= frame_time {
-            // Handle events
-            for event in window.poll_event() {
+            while let Some(event) = window.poll_event() {
                 match event {
                     Event::Closed => window.close(),
-                    _             => {/* do nothing */}
+                    _ => {}
                 }
             }
+
+            window.clear(Color::WHITE);
             
-            // Clear the window
-            window.clear(Color::RED);
-            // Draw the shape
-            window.draw(&circle);
-            // Display things on screen
+            window.draw(&text);
+
             window.display();
             clock.restart();
         }
