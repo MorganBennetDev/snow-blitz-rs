f45bbf25528a27f2281ad10e5af7b3ce296f21d7
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2024-02-24 13:27:05 -0600

    feat: Add frame rate constant

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
-extern crate sfml;
+use sfml::system::{
+    Vector2f,
+    Clock,
+    Time
+};
+use sfml::window::{
+    ContextSettings,
+    VideoMode,
+    Event,
+    Style
+};
+use sfml::graphics::{
+    RenderWindow,
+    RenderTarget,
+    CircleShape,
+    Color,
+    Transformable,
+    Shape
+};
 
-use sfml::system::Vector2f;
-use sfml::window::{ContextSettings, VideoMode, Event, Style};
-use sfml::graphics::{RenderWindow, RenderTarget, CircleShape, Color, Transformable, Shape};
+const FPS: i64 = 30;
 
 fn main() {
+    let frame_time = Time::seconds(1.) / FPS;
+    let mut clock = Clock::start();
+    
     // Create the window of the application
     let mut window : RenderWindow = RenderWindow::new(VideoMode::new(800, 600, 32),
                                                       "SFML Example",
@@ -18,18 +37,22 @@ fn main() {
     circle.set_position(Vector2f::new(100., 100.));
 
     while window.is_open() {
-        // Handle events
-        for event in window.poll_event() {
-            match event {
-                Event::Closed => window.close(),
-                _             => {/* do nothing */}
+        if clock.elapsed_time() >= frame_time {
+            // Handle events
+            for event in window.poll_event() {
+                match event {
+                    Event::Closed => window.close(),
+                    _             => {/* do nothing */}
+                }
             }
+            
+            // Clear the window
+            window.clear(Color::RED);
+            // Draw the shape
+            window.draw(&circle);
+            // Display things on screen
+            window.display();
+            clock.restart();
         }
-        // Clear the window
-        window.clear(Color::RED);
-        // Draw the shape
-        window.draw(&circle);
-        // Display things on screen
-        window.display()
     }
 }
